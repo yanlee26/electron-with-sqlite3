8e993df9cb0df164eba6fb1620b0e5a73e68b746
yanli <yanli@xiaoheiban.cn>
2020-05-14 20:12:23 +0800

    feat: add tray

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {
+  app,
+  Tray,
+  Menu,
+  BrowserWindow
+} = require('electron')
 const path = require('path')
 
-function createWindow () {
+let mainWindow = null;
+
+function createWindow() {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    // frame:false,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
@@ -17,6 +25,12 @@ function createWindow () {
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools()
+
+  mainWindow.on('closed', () => {
+    mainWindow.destroy();
+    app.quit()
+  });
+
 }
 
 // This method will be called when Electron has finished
@@ -24,21 +38,42 @@ function createWindow () {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow()
-  app.on('activate', function () {
+
+  const tray = new Tray('./resources/tray/16x16.png')
+
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: '打开app',
+      click: () => {
+        createWindow()
+      }
+    },
+    {
+      label: '退出',
+      click: () => {
+        mainWindow.close()
+      }
+    }
+  ])
+
+  tray.setContextMenu(contextMenu)
+
+  app.on('activate', function() {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
+
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on('window-all-closed', function() {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit() 
+  if (process.platform !== 'darwin') app.quit()
 })
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 
-app.allowRendererProcessReuse=false
+app.allowRendererProcessReuse = false

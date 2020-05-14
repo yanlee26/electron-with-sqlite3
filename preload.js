65a821d42f73eb29d1d3ee1de016dca337c6a9c0
yanli <yanli@xiaoheiban.cn>
2020-05-14 21:08:08 +0800

    feat: add drag

--- preload.js
+++ preload.js
@@ -1,12 +1,22 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
-const { connect } = require('trilogy')
+const {
+  connect
+} = require('trilogy')
+const {
+  BrowserWindow
+} = require('electron').remote
 
+const drag = require('electron-drag');
 
+// Pass a query selector or a dom element to the function.
+// Dragging the element will drag the whole window.
 const db = connect('./store.db', {
   client: 'sql.js'
 })
-window.addEventListener('DOMContentLoaded', async() => {
+
+window.addEventListener('DOMContentLoaded', async () => {
+  drag('body');
   const users = await db.model('users', {
     id: 'increments',
     name: String,
@@ -25,7 +35,7 @@ window.addEventListener('DOMContentLoaded', async() => {
     comments: 0,
     admin: true
   })
-  
+
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector)
     if (element) element.innerText = text
@@ -37,11 +47,39 @@ window.addEventListener('DOMContentLoaded', async() => {
 
 
 
-  console.log(await users.findOne({ name: 'anon' }))
+  console.log(await users.findOne({
+    name: 'anon'
+  }))
   // -> { id: 1, name: 'anon', comments: 0, admin: false }
 
-  console.log(await users.findOne({ name: 'boss' }))
+  console.log(await users.findOne({
+    name: 'boss'
+  }))
   // -> { id: 2, name: 'boss', comments: 0, admin: true }
 
-  console.log(await users.count({ admin: true }))
+  console.log(await users.count({
+    admin: true
+  }))
+
+  // Minimize task
+  document.getElementById("min-btn").addEventListener("click", (e) => {
+    var window = BrowserWindow.getFocusedWindow();
+    window.minimize();
+  });
+
+  // Maximize window
+  document.getElementById("max-btn").addEventListener("click", (e) => {
+    var window = BrowserWindow.getFocusedWindow();
+    if (window.isMaximized()) {
+      window.unmaximize();
+    } else {
+      window.maximize();
+    }
+  });
+
+  // Close app
+  document.getElementById("close-btn").addEventListener("click", (e) => {
+    var window = BrowserWindow.getFocusedWindow();
+    window.close();
+  });
 })
